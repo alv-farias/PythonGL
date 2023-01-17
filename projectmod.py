f6da9897e3b72987dcd45a78ab1b59890c71f240
Alvaro Farias <alvardev20@gmail.com>
2023-01-17 17:42:21 -0300

    add projectmod

---
+++ projectmod.py
@@ -0,0 +1,228 @@
+import pygame as pg
+from OpenGL.GL import *
+from OpenGL.GL.shaders import compileProgram,compileShader
+import numpy as np
+import pyrr
+
+class Obj:
+
+
+    def __init__(self, position, eulers):
+
+        self.position = np.array(position, dtype=np.float32)
+        self.eulers = np.array(eulers, dtype=np.float32)
+
+class App:
+    def __init__(self):
+        #Inciailização do Pygame
+        pg.init()
+        pg.display.set_mode((640,480), pg.OPENGL|pg.DOUBLEBUF)
+        self.clock = pg.time.Clock()
+        # Inicialização da OpenGL
+        glClearColor(0.1, 0.1, 0.1, 0.1)
+        self.obj_mesh = Mesh("modelos3D/intestino.obj")
+        self.shader = self.createShader("shaders/vertex.txt", "shaders/fragment.txt")
+        glUseProgram(self.shader)
+        glUniform1i(glGetUniformLocation(self.shader, "imageTexture"), 0)
+        glEnable(GL_DEPTH_TEST)
+
+        self.textura_tijolo = Textura("texturas/textura1.png")
+
+        self.obj = Obj(
+            position = [0,0,-3],
+            eulers = [0,0,0]
+        )
+        #Configuração das perspectivas de projeção
+        projection_transform = pyrr.matrix44.create_perspective_projection(
+            fovy = 45, aspect = 640/480, 
+            near = 0.1, far = 10, dtype=np.float32
+        )
+        glUniformMatrix4fv(
+            glGetUniformLocation(self.shader,"projection"),
+            1, GL_FALSE, projection_transform
+        )
+        self.modelMatrixLocation = glGetUniformLocation(self.shader,"model")
+        self.mainLoop()
+
+    def createShader(self, vertexFilepath, fragmentFilepath):
+        # Ler as informações dos shaders
+        with open(vertexFilepath,'r') as f:
+            vertex_src = f.readlines()
+
+        with open(fragmentFilepath,'r') as f:
+            fragment_src = f.readlines()
+        
+        shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER),
+                                compileShader(fragment_src, GL_FRAGMENT_SHADER))
+        
+        return shader
+
+    def mainLoop(self):
+        running = True
+        while (running):
+            #Checar se a janela será fechada
+            for event in pg.event.get():
+                if (event.type == pg.QUIT):
+                    running = False
+            
+            #movimentação do cubo
+            self.obj.eulers[2] -= 0.25
+            if self.obj.eulers[2] < 360:
+                self.obj.eulers[2] += 360
+            
+            #Atualização da tela
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+            glUseProgram(self.shader)
+
+            # Aplicação de translação
+            model_transform = pyrr.matrix44.create_identity(dtype=np.float32)
+            model_transform = pyrr.matrix44.multiply(
+                m1=model_transform, 
+                m2=pyrr.matrix44.create_from_eulers(
+                    eulers=np.radians(self.obj.eulers), dtype=np.float32
+                )
+            )
+            model_transform = pyrr.matrix44.multiply(
+                m1=model_transform, 
+                m2=pyrr.matrix44.create_from_translation(
+                    vec=np.array(self.obj.position),dtype=np.float32
+                )
+            )
+            glUniformMatrix4fv(self.modelMatrixLocation,1,GL_FALSE,model_transform)
+            self.textura_tijolo.use()
+            glBindVertexArray(self.obj_mesh.vao)
+            glDrawArrays(GL_TRIANGLES, 0, self.obj_mesh.vertex_count)
+
+            pg.display.flip()
+
+            #Controle do frame
+            self.clock.tick(60)
+        self.quit()
+
+    def quit(self):
+        self.obj_mesh.apaga()
+        self.textura_tijolo.apaga()
+        glDeleteProgram(self.shader)
+        pg.quit()
+
+class Mesh:
+    def __init__(self, filename):
+        #Carrega o arquivo para verificar os compontentes do mesmo (v, vt, vn, f)
+        self.vert = self.loadMesh(filename)
+        self.vertex_count = len(self.vert)//8
+        self.vert = np.array(self.vert, dtype=np.float32)
+
+        self.vao = glGenVertexArrays(1)
+        glBindVertexArray(self.vao)
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        glBufferData(GL_ARRAY_BUFFER, self.vert.nbytes, self.vert, GL_STATIC_DRAW)
+        #posição
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
+        #textura
+        glEnableVertexAttribArray(1)
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(12))
+    
+    def loadMesh(self, filename):
+
+        #Dados fornecidos no arquivo objeto
+        v = []
+        vt = []
+        vn = []
+        
+        vertices = []
+
+        #open the obj file and read the data
+        with open(filename,'r') as f:
+            line = f.readline()
+            while line:
+                firstSpace = line.find(" ")
+                flag = line[0:firstSpace]
+                if flag=="v":
+                    #Carrega os vertex
+                    line = line.replace("v ","")
+                    line = line.split(" ")
+                    l = [float(x) for x in line]
+                    v.append(l)
+                elif flag=="vt":
+                    #Carrega as coordenadas das texturas
+                    line = line.replace("vt ","")
+                    line = line.split(" ")
+                    l = [float(x) for x in line]
+                    vt.append(l)
+                elif flag=="vn":
+                    #Carrega as normais
+                    line = line.replace("vn ","")
+                    line = line.split(" ")
+                    l = [float(x) for x in line]
+                    vn.append(l)
+                elif flag=="f":
+                    #Carrega as faces
+                    line = line.replace("f ","")
+                    line = line.replace("\n","")
+                    #Carrega os vertices de cada linha
+                    line = line.split(" ")
+                    faceVertices = []
+                    faceTextures = []
+                    faceNormals = []
+                    for vertex in line:
+                        #break out into [v,vt,vn],
+                        #correct for 0 based indexing.
+                        l = vertex.split("/")
+                        position = int(l[0]) - 1
+                        faceVertices.append(v[position])
+                        texture = int(l[1]) - 1
+                        faceTextures.append(vt[texture])
+                        normal = int(l[2]) - 1
+                        faceNormals.append(vn[normal])
+                    
+                    triangles_in_face = len(line) - 2
+
+                    vertex_order = []
+                    
+                    for i in range(triangles_in_face):
+                        vertex_order.append(0)
+                        vertex_order.append(i+1)
+                        vertex_order.append(i+2)
+                    for i in vertex_order:
+                        for x in faceVertices[i]:
+                            vertices.append(x)
+                        for x in faceTextures[i]:
+                            vertices.append(x)
+                        for x in faceNormals[i]:
+                            vertices.append(x)
+                line = f.readline()
+        return vertices
+    
+    def apaga(self):
+        glDeleteVertexArrays(1, (self.vao,))
+        glDeleteBuffers(1,(self.vbo,))
+
+class Textura:
+
+    
+    def __init__(self, filepath):
+        # inicia a textura
+        self.texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        imagem = pg.image.load(filepath).convert()
+        imageWidth,imageHeight = imagem.get_rect().size
+        ## OpenGL não entende o tipo de dados do Pygame logo é feita uma conversão da img para uma string
+        imgData = pg.image.tostring(imagem,'RGBA')
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,imageWidth,imageHeight,0,GL_RGBA,GL_UNSIGNED_BYTE,imgData)
+        glGenerateMipmap(GL_TEXTURE_2D)
+
+    def use(self):
+        # Ativa a textura 
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D,self.texture)
+
+    def apaga(self):
+        glDeleteTextures(1, (self.texture,))
+
+myApp = App()
